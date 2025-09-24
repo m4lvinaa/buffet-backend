b437471f9e52357ea032c69c2f9d0115aa8f2180
Malvina <malvina.florencia2003@gmail.com>
2025-09-24 01:03:02 -0300

    Agrega carpeta routes y prueba de conexion con base de datos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -8,14 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Importar rutas
+const testRoutes = require('./routes/test');
+app.use('/', testRoutes);
+
+// Ruta raíz
 app.get('/', (req, res) => {
-    res.send('Servidor del buffet universitario funcionando');
+  res.send('Servidor del buffet universitario funcionando');
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
-
-
-
-
+});
