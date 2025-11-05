cdea90c09fd5053bd0bc44e223cff44648d15e7d
Brisa Valero <138631888+Brisa707@users.noreply.github.com>
2025-11-05 19:46:33 -0300

    Script para que el Admin actualice el estado del pedido
    Script

--- db/estado_pedido.sql
+++ db/estado_pedido.sql
@@ -1,9 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;  
+
 CREATE TABLE estado_pedido (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   pedido_id UUID NOT NULL REFERENCES pedidos(id) ON DELETE CASCADE,
   estado_anterior TEXT,
   estado_actual TEXT NOT NULL,
-  administrador UUID,      
-  motivo_de_cambio TEXT, 
+  administrador UUID REFERENCES usuarios(id),    
+  motivo_de_cambio TEXT,    
   fecha_hora TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
