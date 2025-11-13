bc136ae68d3ec63211f7647dfba2a1e5c0ae550d
Julieta <julysayago12@gmail.com>
2025-11-13 10:29:03 -0300

    try to fix cors

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,18 @@
-module.exports = {
-  origin: true, // acepta cualquier origen
+const allowedOrigins = [
+  "http://localhost:5173", // frontend local
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
 };
+
+module.exports = corsOptions;
