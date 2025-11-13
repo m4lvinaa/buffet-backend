52bfa0973bd14d0de2b8c1e5f708a178e39c9e84
Julieta <julysayago12@gmail.com>
2025-11-13 09:30:29 -0300

    cambie dominio por cors

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: [
-    'https://buffet-ecommerce-2t4sk4d1pj-brisa-valerio5s-projects.vercel.app',
-    'https://buffet-ecommerce-julietas-projects-bb92a50b.vercel.app',
-    'http://localhost:5173'
+    "https://buffet-ecommerce-2t4sk4d1pj-brisa-valerio5s-projects.vercel.app",
+    "https://buffet-ecommerce-two.vercel.app",
+    "http://localhost:5173",
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 
 // Middleware CORS
@@ -21,17 +21,17 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Rutas...
-app.use('/api/usuarios', require('./routes/usuarios'));
-app.use('/api/productos', require('./routes/productos'));
-app.use('/api/carrito', require('./routes/carrito'));
-app.use('/api/pedidos', require('./routes/pedido'));
-app.use('/api/dashboard', require('./routes/dashboard'));
-app.use('/api/categorias', require('./routes/categorias'));
-app.use('/api', require('./routes/upload'));
-app.use('/api/notificaciones', require('./routes/notificaciones'));
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/productos", require("./routes/productos"));
+app.use("/api/carrito", require("./routes/carrito"));
+app.use("/api/pedidos", require("./routes/pedido"));
+app.use("/api/dashboard", require("./routes/dashboard"));
+app.use("/api/categorias", require("./routes/categorias"));
+app.use("/api", require("./routes/upload"));
+app.use("/api/notificaciones", require("./routes/notificaciones"));
 
-app.get('/', (req, res) => {
-  res.send('Servidor del buffet universitario funcionando');
+app.get("/", (req, res) => {
+  res.send("Servidor del buffet universitario funcionando");
 });
 
 app.listen(PORT, () => {
