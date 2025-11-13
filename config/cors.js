3ed6b5400bc8e2be9860c45db24fb1daad1998b7
Julieta <julysayago12@gmail.com>
2025-11-13 10:06:36 -0300

    probar que cors acepte cualquier origen

--- config/cors.js
+++ config/cors.js
@@ -1,17 +1,5 @@
-const allowedOrigins = [
-  "https://buffet-ecommerce-two.vercel.app",
-  "https://buffet-ecommerce-2t4sk4d1pj-brisa-valerio5s-projects.vercel.app",
-  "http://localhost:5173",
-];
-
 module.exports = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("No permitido por CORS"));
-    }
-  },
+  origin: true, // acepta cualquier origen
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
