8b801417ce95ef44f579e763f3e2100e65d69c30
Julieta <julysayago12@gmail.com>
2025-11-13 10:00:37 -0300

    create cors.js

--- app.js
+++ app.js
@@ -2,29 +2,10 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const corsOptions = require("./config/cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Lista de orígenes permitidos
-const allowedOrigins = [
-  "https://buffet-ecommerce-two.vercel.app",
-  "https://buffet-ecommerce-2t4sk4d1pj-brisa-valerio5s-projects.vercel.app",
-  "http://localhost:5173",
-];
-
-// Configuración CORS
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("No permitido por CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
 
 // Middleware CORS
 app.use(cors(corsOptions));
